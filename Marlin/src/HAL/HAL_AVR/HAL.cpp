22fda76fdcf1f4d35a1d40ad5250787dc2efeffd
InsanityAutomation <38436470+InsanityAutomation@users.noreply.github.com>
2019-07-11 22:43:51 -0400

    Update HAL.cpp

--- Marlin/src/HAL/HAL_AVR/HAL.cpp
+++ Marlin/src/HAL/HAL_AVR/HAL.cpp
@@ -38,16 +38,16 @@
 void HAL_init(void) {
   // Init Servo Pins
   #if PIN_EXISTS(SERVO0)
-    OUT_WRITE(SERVO0_PIN, LOW);
+    //OUT_WRITE(SERVO0_PIN, LOW);
   #endif
   #if PIN_EXISTS(SERVO1)
-    OUT_WRITE(SERVO1_PIN, LOW);
+    //OUT_WRITE(SERVO1_PIN, LOW);
   #endif
   #if PIN_EXISTS(SERVO2)
-    OUT_WRITE(SERVO2_PIN, LOW);
+   //OUT_WRITE(SERVO2_PIN, LOW);
   #endif
   #if PIN_EXISTS(SERVO3)
-    OUT_WRITE(SERVO3_PIN, LOW);
+    //OUT_WRITE(SERVO3_PIN, LOW);
   #endif
 }
 
