fd4358005ff0323176626ec0e1b895591171b20f
InsanityAutomation <d.menzel@insanityautomation.com>
2019-08-03 21:51:02 -0400

    Add additional TFT types

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1399,10 +1399,10 @@
 // Therefore some clients abort after 30 seconds in a timeout.
 // Some other clients start sending commands while receiving a 'wait'.
 // This "wait" is only sent when the buffer is empty. 1 second is a good value here.
-#define NO_TIMEOUTS 1000 // Milliseconds
+#define NO_TIMEOUTS 0 // Milliseconds
 
 // Some clients will have this feature soon. This could make the NO_TIMEOUTS unnecessary.
-#define ADVANCED_OK
+//#define ADVANCED_OK
 
 // Printrun may have trouble receiving long strings all at once.
 // This option inserts short delays between lines of serial output.
