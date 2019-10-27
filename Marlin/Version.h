f3f269fdf34b0af3fc2c19f8f8d3d5caf8436595
InsanityAutomation <d.menzel@insanityautomation.com>
2019-10-27 13:22:57 -0400

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.0.x_SX4H"
+#define SHORT_BUILD_VERSION "2.0.x_SX4I"
 
 /**
  * Verbose version identifier which should contain a reference to the location
