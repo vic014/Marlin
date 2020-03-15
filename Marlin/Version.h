fb4a1453a47af3243339567cad3fdc0665c6933b
InsanityAutomation <d.menzel@insanityautomation.com>
2020-03-15 14:02:47 -0400

    Version and babystep limit

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.0.3_SX4K"
+#define SHORT_BUILD_VERSION "2.0.5_SX4L"
 
 /**
  * Verbose version identifier which should contain a reference to the location
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2020-02-16"
+#define STRING_DISTRIBUTION_DATE "2020-03-15"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
